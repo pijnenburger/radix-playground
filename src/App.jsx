ff4ab233b35b6125012e254580398728158041eb
Rick Pijnenburg <rpijnenburg92@gmail.com>
2023-05-31 14:41:58 +0200

    Simple working versions
    Still have to make resultCount variable.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,40 @@ import QuickFilter from "./QuickFilter";
 import "./App.css";
 import brands from "./json/brands.json";
 import closingDates from "./json/closingDates.json";
+import movements from "./json/movements.json";
+import reservePrice from "./json/reservePrice.json";
+import locations from "./json/locations.json";
 
 function App() {
   return (
-    <div className=" flex justify-start gap-3 bg-slate-100 px-8 py-40 font-sans">
-      <QuickFilter filterValues={brands} filterLabel="Brand" />
-      <QuickFilter filterValues={closingDates} filterLabel="Closing date" />
+    <div>
+      <div
+        id="sticky-bar"
+        className=" sticky top-0 flex w-full justify-center border-b bg-white "
+      >
+        <div
+          id="scroll-container"
+          className=" flex w-[984px] max-w-[984px] justify-start overflow-clip overflow-x-auto whitespace-nowrap px-4 py-4 font-sans"
+        >
+          <div className="flex justify-start gap-2">
+            <QuickFilter
+              filterValues={reservePrice}
+              filterLabel="Reserve price"
+            />
+            <QuickFilter
+              filterValues={closingDates}
+              filterLabel="Closing date"
+            />
+            <QuickFilter filterValues={locations} filterLabel="Location" />
+            <QuickFilter filterValues={brands} filterLabel="Brand" />
+            <QuickFilter
+              filterValues={movements}
+              filterLabel="Watch movement"
+            />
+          </div>
+        </div>
+      </div>
+      <div id="content" className=" h-[300vh] bg-slate-50"></div>
     </div>
   );
 }
--- src/QuickFilter.jsx
+++ src/QuickFilter.jsx
@@ -18,104 +18,113 @@ function QuickFilter({ filterValues, filterLabel }) {
   const filterCount = checkedValues.length;
 
   function addOrRemoveCheck(item) {
-    if (checkedValues.includes(item)) {
-      const nextItems = checkedValues.filter((a) => a !== item);
+    // Complex function looking at label & count. Currently not working...
+    const itemExists = checkedValues.some((checkedItem) => {
+      return (
+        checkedItem.label === item.label && checkedItem.count === item.count
+      );
+    });
+    console.log(item);
+    console.log(itemExists);
+
+    // Simple function checking labels only
+    if (checkedValues.includes(item.label)) {
+      const nextItems = checkedValues.filter((a) => a !== item.label);
       setCheckedValues(nextItems);
     } else {
-      const nextItems = [...checkedValues, item];
+      const nextItems = [...checkedValues, item.label];
       setCheckedValues(nextItems);
     }
   }
 
-  const [isOpen, setIsOpen] = React.useState();
-
-  // const ref = React.useRef();
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <>
-      <DropdownMenu.Root
-        className="flex justify-center"
-        onOpenChange={(event) => {
-          setIsOpen(event);
-        }}
+    <DropdownMenu.Root
+      className=" flex justify-center"
+      onOpenChange={(event) => {
+        setIsOpen(event);
+      }}
+      open={isOpen}
+    >
+      <DropdownMenu.Trigger
+        className={` relative flex select-none items-center justify-center gap-1 bg-white py-2 pl-4 pr-3 text-base text-slate-800 hover:border-slate-400 hover:bg-slate-100 hover:text-slate-900 ${
+          filterCount === 0
+            ? "border border-slate-300"
+            : "border-2 border-slate-900"
+        }`}
       >
-        <DropdownMenu.Trigger
-          className={`relative flex select-none items-center justify-center gap-1 border-2 border-slate-300 bg-white text-base text-slate-800 hover:border-slate-400 hover:bg-slate-100 hover:text-slate-900 ${
-            filterCount === 0 ? "border-slate-400" : "border-slate-900"
-          }`}
+        {filterCount === 1 ? checkedValues[0] : filterLabel}
+        {filterCount >= 1 && <FilterIndicator value={filterCount} />}
+        <ChevronDownIcon
+          className={`${
+            isOpen ? "rotate-180" : "rotate-0"
+          } h-4 w-4 transform transition-transform will-change-transform`}
+        />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal className="relative">
+        <DropdownMenu.Content
+          align="start"
+          sideOffset={4}
+          className={` max-h-[320px] min-w-[320px] overflow-scroll border border-slate-300 bg-white p-0 text-slate-800 shadow-sm ${styles.DropdownMenuContent}`}
         >
-          {filterCount === 1 ? checkedValues[0] : filterLabel}
-          {filterCount >= 1 && <FilterIndicator value={filterCount} />}
-          <ChevronDownIcon
-            className={`${
-              isOpen ? "rotate-180" : "rotate-0"
-            } h-4 w-4 transform transition-transform will-change-transform`}
-          />
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Portal className="relative">
-          <DropdownMenu.Content
-            align="start"
-            sideOffset={4}
-            className={` max-h-[320px] min-w-[320px] overflow-scroll border border-slate-300 bg-white p-0 text-slate-800 shadow-sm ${styles.DropdownMenuContent}`}
-          >
-            {filterValues.map(({ label, count }) => {
-              const id = crypto.randomUUID();
-              const checked = checkedValues.includes(label);
+          {filterValues.map((item) => {
+            const id = crypto.randomUUID();
+            const checked = checkedValues.includes(item.label);
 
-              return (
-                <div key={id}>
-                  <DropdownMenu.CheckboxItem
-                    key={`label-${id}`}
-                    checked={checked}
-                    onSelect={(event) => {
-                      event.preventDefault();
-                    }}
-                    onCheckedChange={() => {
-                      addOrRemoveCheck(label);
-                    }}
-                    className={` flex cursor-pointer flex-row gap-2 text-ellipsis px-4 py-6 font-sans text-base text-slate-800 data-[highlighted]:bg-slate-50 data-[highlighted]:text-slate-900 data-[state=checked]:text-blue-700 ${styles.DropdownMenuCheckboxItem}`}
+            return (
+              <div key={id}>
+                <DropdownMenu.CheckboxItem
+                  key={`label-${id}`}
+                  checked={checked}
+                  onSelect={(event) => {
+                    event.preventDefault();
+                  }}
+                  onCheckedChange={() => {
+                    addOrRemoveCheck(item);
+                  }}
+                  className={` flex cursor-pointer flex-row gap-2 text-ellipsis px-4 py-6 font-sans text-base text-slate-800 data-[highlighted]:bg-slate-50 data-[highlighted]:text-slate-900 data-[state=checked]:text-blue-700 ${styles.DropdownMenuCheckboxItem}`}
+                >
+                  <DropdownMenu.Group
+                    className={`flex h-6 w-6 justify-center border align-middle ${
+                      checked
+                        ? "border-blue-600 bg-blue-600 hover:border-blue-700 hover:bg-blue-700"
+                        : "border-slate-400 bg-white hover:border-slate-500 hover:bg-white"
+                    }`}
                   >
-                    <DropdownMenu.Group
-                      className={`flex h-6 w-6 justify-center border align-middle ${
-                        checked
-                          ? "border-blue-600 bg-blue-600 hover:border-blue-700 hover:bg-blue-700"
-                          : "border-slate-400 bg-white hover:border-slate-500 hover:bg-white"
-                      }`}
-                    >
-                      <DropdownMenu.ItemIndicator className={`h-6 w-6 `}>
-                        <CheckIcon className="h-6 w-6 text-white" />
-                      </DropdownMenu.ItemIndicator>
-                    </DropdownMenu.Group>
-                    {label}
-                    <DropdownMenu.Item
-                      className="pointer-events-none ml-auto pl-4 font-mono text-sm text-slate-500"
-                      key={`count-${id}`}
-                    >
-                      {count}
-                    </DropdownMenu.Item>
-                  </DropdownMenu.CheckboxItem>
-                  <DropdownMenu.Separator className=" h-px bg-slate-200" />
-                </div>
-              );
-            })}
-            <button
-              onClick={() => {
-                console.log("close the modal from here");
-              }}
-              className={`sticky bottom-0 box-border flex w-full justify-center rounded-none bg-blue-600 px-3 py-2 align-middle text-base text-white hover:bg-blue-700 
+                    <DropdownMenu.ItemIndicator className={`h-6 w-6 `}>
+                      <CheckIcon className="h-6 w-6 text-white" />
+                    </DropdownMenu.ItemIndicator>
+                  </DropdownMenu.Group>
+                  {item.label}
+                  <DropdownMenu.Group
+                    className="pointer-events-none ml-auto pl-4 font-mono text-sm text-slate-500"
+                    key={`count-${id}`}
+                  >
+                    {item.count}
+                  </DropdownMenu.Group>
+                </DropdownMenu.CheckboxItem>
+                <DropdownMenu.Separator className=" h-px bg-slate-200" />
+              </div>
+            );
+          })}
+          <button
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            className={`sticky bottom-0 box-border flex w-full justify-center rounded-none bg-blue-600 px-3 py-3 align-middle font-sans text-base text-white hover:bg-blue-700 
               ${
                 filterCount === 0 &&
                 "bg-slate-900 text-slate-100 hover:bg-slate-900"
               }`}
-            >
-              {filterCount === 0
-                ? "Close"
-                : `Show ${Math.round(Math.random() * 240, 0)} results`}
-            </button>
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
-    </>
+          >
+            {filterCount === 0
+              ? "Close"
+              : `Show ${Math.round(Math.random() * 240, 0)} results`}
+          </button>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
 
