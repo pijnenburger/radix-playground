96932b381e2d35e91080bf03afaec409edcddb4a
Rick Pijnenburg <rpijnenburg92@gmail.com>
2023-06-01 14:06:24 +0200

    continuation on input validation

--- src/QuickNumFilter.jsx
+++ src/QuickNumFilter.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import * as Form from "@radix-ui/react-form";
+import * as Slider from "@radix-ui/react-slider";
 
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 
@@ -9,16 +10,20 @@ function QuickNumFilter({ filterLabel }) {
   const [minPrice, setMinPrice] = React.useState("");
   const [maxPrice, setMaxPrice] = React.useState("");
   const [filterApplied, setFilterApplied] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(false);
 
   function validateOnBlur() {
     if (maxPrice < minPrice && maxPrice !== "") {
-      setMaxPrice(parseInt(minPrice) + 1);
+      // setMaxPrice(parseInt(minPrice) + 1);
+      setDisabled(true);
       setFilterApplied(false);
     }
 
     if (minPrice === "" && maxPrice === "") {
+      setDisabled(false);
       setFilterApplied(false);
     } else {
+      setDisabled(false);
       setFilterApplied(true);
     }
   }
@@ -38,7 +43,8 @@ function QuickNumFilter({ filterLabel }) {
             : "border-2 border-slate-900"
         }`}
       >
-        {!filterApplied && filterLabel}
+        {filterLabel}
+        {/* {!filterApplied && filterLabel}
         {filterApplied &&
           minPrice !== "" &&
           maxPrice === "" &&
@@ -50,7 +56,7 @@ function QuickNumFilter({ filterLabel }) {
         {filterApplied &&
           minPrice !== "" &&
           maxPrice !== "" &&
-          `€ ${minPrice} - € ${maxPrice}`}
+          `€ ${minPrice} - € ${maxPrice}`} */}
         <ChevronDownIcon
           width={20}
           height={20}
@@ -71,15 +77,26 @@ function QuickNumFilter({ filterLabel }) {
                 {/* Mininum price input field */}
                 <Form.Field className="flex flex-col gap-2" name="min_price">
                   <div className="flex items-baseline justify-between">
-                    <Form.Label className="font-sans text-base font-medium text-slate-900">
+                    <Form.Label className="font-sans text-base text-slate-900">
                       From
                     </Form.Label>
+                    {/* {minPrice !== "" && (
+                      <button
+                        onClick={() => {
+                          setMinPrice("");
+                        }}
+                        className="flex h-fit justify-center rounded-none border-none bg-transparent p-0 align-middle font-sans text-base text-blue-500"
+                      >
+                        clear
+                      </button>
+                    )} */}
                   </div>
                   <div className="relative">
                     <Form.Control asChild>
                       <input
-                        className="box-border inline-flex h-[40px] w-full appearance-none items-center justify-center rounded border bg-slate-100 pl-8 pr-4 font-sans text-base leading-none text-slate-900 outline-none selection:bg-blue-600 selection:text-white hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
+                        className="box-border inline-flex h-[40px] w-full appearance-none items-center justify-center rounded border bg-slate-100 pl-8 pr-2 font-sans text-base leading-none text-slate-900 outline-none selection:bg-blue-600 selection:text-white hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                         type="number"
+                        step={1}
                         value={minPrice}
                         // max={maxPrice}
                         placeholder="No min"
@@ -93,7 +110,7 @@ function QuickNumFilter({ filterLabel }) {
                     </span>
                   </div>
                   <Form.Message
-                    className=" text-sm text-red-800"
+                    className=" text-sm text-red-700"
                     match="rangeOverflow"
                   >
                     Min should be lower than max
@@ -110,8 +127,9 @@ function QuickNumFilter({ filterLabel }) {
                   <div className="relative">
                     <Form.Control asChild>
                       <input
-                        className="box-border inline-flex h-[40px] w-full appearance-none items-center justify-center rounded border bg-slate-100 pl-8 pr-4 font-sans text-base leading-none text-slate-900 outline-none selection:bg-blue-600 selection:text-white hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
+                        className="box-border inline-flex h-[40px] w-full appearance-none items-center justify-center rounded border bg-slate-100 pl-8 pr-2 font-sans text-base leading-none text-slate-900 outline-none selection:bg-blue-600 selection:text-white hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black]"
                         type="number"
+                        step={1}
                         value={maxPrice}
                         min={minPrice}
                         placeholder="No max"
@@ -125,33 +143,57 @@ function QuickNumFilter({ filterLabel }) {
                     </span>
                   </div>
                   <Form.Message
-                    className="text-sm text-red-800"
+                    className="text-sm text-red-700"
                     match="rangeUnderflow"
                   >
                     Max should be higher than min
                   </Form.Message>
                 </Form.Field>
               </div>
+
+              {/* Slider */}
+              {/* <Slider.Root
+                defaultValue={[25, 75]}
+                step={10}
+                minStepsBetweenThumbs={1}
+                className="relative flex h-5 w-[200px] touch-none select-none items-center"
+              >
+                <Slider.Track>
+                  <Slider.Range />
+                </Slider.Track>
+                <Slider.Thumb className=" block" />
+                <Slider.Thumb />
+              </Slider.Root> */}
               <Form.Submit
                 onSubmit={(event) => {
                   event.preventDefault();
                   setIsOpen(false);
-                  setFilterApplied((currentValue) => !currentValue);
+
+                  if ((minPrice !== "") | (maxPrice !== "")) {
+                    setFilterApplied(true);
+                  }
+                  // setFilterApplied((currentValue) => !currentValue);
                 }}
                 asChild
               >
                 <button
+                  disabled={disabled}
                   onClick={(event) => {
                     event.preventDefault();
                     setIsOpen(false);
-                    setFilterApplied((currentValue) => !currentValue);
+
+                    if ((minPrice !== "") | (maxPrice !== "")) {
+                      // setFilterApplied(true);
+                    }
+                    // setFilterApplied((currentValue) => !currentValue);
                   }}
                   className={` box-border flex w-full justify-center rounded-none bg-blue-600 px-3 py-3 align-middle font-sans text-base text-white hover:bg-blue-700 
             ${
               !filterApplied && "bg-slate-900 text-slate-100 hover:bg-slate-900"
             }`}
                 >
-                  {!filterApplied ? "Close" : `Show 1248 results`}
+                  {/* {!filterApplied ? "Close" : `Show 1248 results`} */}
+                  Apply
                 </button>
               </Form.Submit>
             </Form.Root>
